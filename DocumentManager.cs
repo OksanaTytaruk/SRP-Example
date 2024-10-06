29127348f1ef8a3be5eb2e896c826c5f8b694932
OksanaTytaruk <cassieroyster@gmail.com>
2024-10-06 23:01:48 +0300

    Create DocumentManager.cs

---
+++ DocumentManager.cs
@@ -0,0 +1,15 @@
+public class DocumentDatabase
+{
+    public void Save(Document document)
+    {
+        // Логіка збереження документа в базу даних
+    }
+}
+
+public class DocumentPrinter
+{
+    public void Print(Document document)
+    {
+        // Логіка друку документа
+    }
+}
